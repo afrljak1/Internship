72defa3043466d12d64dea5434449d83e3e36f7d
afrljak1 <afrljak1@etf.unsa.ba>
2023-08-21 23:28:34 +0200

    implementiran api

---
+++ Movie-Api/Controllers/MovieController.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Movie_Api.Models;
+
+namespace Movie_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class MovieController : ControllerBase
+    {
+        private readonly APIDbContext _context;
+
+        public MovieController(APIDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Movies
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Movie>>> GetMovie()
+        {
+            return await _context.Movies.ToListAsync();
+        }
+
+        // GET: api/Movie/1
+        [HttpGet("{genre}")]
+        public async Task<ActionResult<Movie>> GetMoviet(string genre)
+        {
+            var movie = await _context.Movies.FirstOrDefaultAsync(m => m.Genre == genre);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return movie;
+        }
+
+        [HttpGet("search")]
+        public async Task<ActionResult<IEnumerable<Movie>>> SearchMoviessByName(string name)
+        {
+            var movies = await _context.Movies
+                .Where(s => s.Title.Contains(name))
+                .ToListAsync();
+
+            if (movies == null || movies.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(movies);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Movie), 201)]
+        public async Task<ActionResult<Movie>> PostMovie(Movie movie)
+        {
+            _context.Movies.Add(movie);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMovie", new { id = movie.MovieId }, movie);
+        }
+
+
+        [HttpDelete("{name}")]
+        public async Task<IActionResult> DeleteMovie(string name)
+        {
+            var movie = await _context.Movies.FirstOrDefaultAsync(m => m.Title == name);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            _context.Movies.Remove(movie);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(typeof(Movie), 204)]
+        public async Task<IActionResult> PutMovie(int id, Movie movie)
+        {
+            if (id != movie.MovieId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(movie).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MovieExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent(); //204 No Content
+        }
+
+        private bool MovieExists(int id)
+        {
+            return _context.Movies.Any(e => e.MovieId == id);
+        }
+
+    }
+}
---
+++ Movie-Api/Models/APIDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Movie_Api.Models
+{
+    public class APIDbContext : DbContext
+    {
+        public APIDbContext(DbContextOptions<APIDbContext> options) : base(options)
+        { }
+
+
+        public DbSet<Movie> Movies { get; set; }
+
+    }
+    }
