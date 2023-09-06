436346f3c5596f9b623b0a1c611b8050c50fa7df
afrljak1 <afrljak1@etf.unsa.ba>
2023-09-06 22:08:35 +0200

    wpf app - movie app

---
+++ MovieApp/ApiHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MovieApp
+{
+    public static class ApiHelper
+    {
+        private const string apiBaseUrl = "https://localhost:7064/api/Movie/";
+
+        public static async Task UpdateMovieAsync(MovieInfo movie)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string url = $"{apiBaseUrl}{movie.MovieId}";
+                    string json = JsonConvert.SerializeObject(movie);
+                    StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    HttpResponseMessage response = await client.PutAsync(url, content);
+                    response.EnsureSuccessStatusCode();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error updating movie: {ex.Message}");
+            }
+        }
+    }
+}
